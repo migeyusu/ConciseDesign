d3bb2684cb4f4062404c9f2bf5ba266a4442124e
朱杰 <981975491@qq.com>
2017-09-18 18:06:07 +0800

    添加项目文件。

---
+++ ConciseDesign/Controls/DialogHostControl.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ConciseDesign.WPF.Controls
+{
+    /* dialog调用分为两种形式：
+     * 1.原始形式：返回command execute的callback
+     * 2.高级形式：返回true/false 并返回execute的parameter作为 out result
+     * 要么使用无返回的callback，要么使用返回的out result，同时使用返回值和callback不妥
+     */
+
+    /// <summary>
+    /// DialogHostControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class DialogHostControl : UserControl
+    {
+        public const string OpenDialogState = "OpenDialog";
+
+        public const string CloseDialogState = "CloseDialog";
+        
+        public object DialogContent
+        {
+            get { return (object)GetValue(DialogContentProperty); }
+            set { SetValue(DialogContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty DialogContentProperty =
+            DependencyProperty.Register("DialogContent", typeof(object), typeof(DialogHostControl), new PropertyMetadata(default(object)));
+
+        private ExecutedRoutedEventHandler _executedRoutedEventHandler;
+
+        //第一类base dialog
+        public void RaiseDialog(object content, ExecutedRoutedEventHandler handler)
+        {
+            DialogContent = content;
+            _executedRoutedEventHandler = handler;
+            VisualStateManager.GoToState(this, OpenDialogState, true);
+        }
+
+        private  AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
+
+        private bool _dialogResult;
+
+        private object _dialogTransferObject = null;
+
+        public Task<bool> RaiseDialogAsync(object content)
+        {
+            _autoResetEvent=new AutoResetEvent(false);
+            DialogContent = content;
+#if past
+            VisualStateManager.GoToState(this, OpenDialogState, true);
+            return Task.Run(() =>
+            {
+                _autoResetEvent.WaitOne();
+                return _dialogResult;
+            });
+#else
+            var task = Task.Run(() => {
+                _dialogResult = false;
+                _autoResetEvent.WaitOne();
+                return _dialogResult;
+            });
+            VisualStateManager.GoToState(this, OpenDialogState, true);
+            return task;
+#endif
+        }
+
+        public async Task<bool> RaiseMessageAsync(string msg)
+        {
+            return await RaiseDialogAsync(new MessageDialog() {DataContext = msg});
+        }
+
+        public async Task<bool> RaiseSubmitTaskAsync(string msg)
+        {
+            return await RaiseDialogAsync(new ConfirmDialog() {Description = msg});
+        }
+
+        public DialogHostControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenDialog_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, OpenDialogState, true);
+        }
+
+        private void CloseDialog_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, CloseDialogState, true);
+            _executedRoutedEventHandler?.Invoke(sender, e);
+            _executedRoutedEventHandler = null;
+            DialogContent = null;
+        }
+
+        private void CancelDialog_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, CloseDialogState, true);
+            //_executedRoutedEventHandler?.Invoke(sender, e);
+            //_executedRoutedEventHandler = null;
+            _dialogTransferObject = e.Parameter;
+            _dialogResult = false;
+            _autoResetEvent.Set();
+            _autoResetEvent.Dispose(); 
+            DialogContent = null;
+            
+        }
+
+        private void SubmitDialog_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, CloseDialogState, true);
+           
+            //_executedRoutedEventHandler?.Invoke(sender, e);
+            //_executedRoutedEventHandler = null;
+            _dialogTransferObject = e.Parameter;
+            _dialogResult = true;
+            _autoResetEvent.Set();
+            _autoResetEvent.Dispose();
+            DialogContent = null;
+        }
+    }
+}
---
+++ ConciseDesign/Controls/DialogRegister.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Concurrent;
+using System.Windows;
+
+namespace ConciseDesign.WPF.Controls
+{
+    /*2017.8 本类也可以使用behavior实现*/
+
+    /// <summary>
+    /// 对话框分发器
+    /// </summary>
+    public static class DialogRegister
+    {
+        private static readonly ConcurrentDictionary<string, DialogHostControl> DialogHostControls =
+            new ConcurrentDictionary<string, DialogHostControl>();
+
+        public static readonly DependencyProperty RegisterProperty = DependencyProperty.RegisterAttached(
+            "Register", typeof(string), typeof(DialogRegister), new PropertyMetadata(default(string), RegisterPropertyChangedCallback));
+
+        private static void RegisterPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if (dependencyPropertyChangedEventArgs.OldValue != null)
+            {
+                DialogHostControls.TryRemove(dependencyPropertyChangedEventArgs.OldValue.ToString(),
+                    out DialogHostControl control);
+            }
+            if (dependencyPropertyChangedEventArgs.NewValue != null) {
+                DialogHostControls.TryAdd(dependencyPropertyChangedEventArgs.NewValue.ToString(),
+                    dependencyObject as DialogHostControl);
+            }
+        }
+
+        /// <summary>
+        /// 注册对话框id
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="value"></param>
+        public static void SetRegister(DialogHostControl element, string value)
+        {
+            element.SetValue(RegisterProperty, value);
+        }
+
+        public static string GetRegister(DialogHostControl element)
+        {
+            return (string) element.GetValue(RegisterProperty);
+        }
+
+        public static DialogHostControl GetById(string id)
+        {
+            return DialogHostControls.TryGetValue(id, out DialogHostControl value) ? value : null;
+        }
+
+    }
+}
