6996a0aecc6b9b965e27091456ea8d0d1dc502d3
Karl.Zhu <981975491@qq.com>
2021-04-14 13:58:35 +0800

    implement progressbar

--- ConciseDesign.WPF/CustomControls/CircularProgressBar.cs
+++ ConciseDesign.WPF/CustomControls/CircularProgressBar.cs
@@ -4,38 +4,79 @@ using System.Windows.Media;
 
 namespace ConciseDesign.WPF.CustomControls
 {
-
-    public class CircularProgressBar : Control
+    public class CircularProgressBar : ProgressBar
     {
         static CircularProgressBar()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(CircularProgressBar), new FrameworkPropertyMetadata(typeof(CircularProgressBar)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CircularProgressBar),
+                new FrameworkPropertyMetadata(typeof(CircularProgressBar)));
+        }
+
+        public CircularProgressBar() { }
+
+        protected override void OnMaximumChanged(double oldMaximum, double newMaximum)
+        {
+            base.OnMaximumChanged(oldMaximum, newMaximum);
+            PercentageValue = ProcessValue(newMaximum, this.Minimum, this.Value);
+        }
+
+        protected override void OnMinimumChanged(double oldMinimum, double newMinimum)
+        {
+            base.OnMinimumChanged(oldMinimum, newMinimum);
+            PercentageValue = ProcessValue(this.Maximum, newMinimum, this.Value);
+        }
+
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            base.OnValueChanged(oldValue, newValue);
+            PercentageValue = ProcessValue(this.Maximum, this.Minimum, newValue);
         }
 
 
-
-        public Brush HeaderBrush
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="maximum"></param>
+        /// <param name="minimum"></param>
+        /// <param name="value"></param>
+        /// <returns>percentage value</returns>
+        private static double ProcessValue(double maximum, double minimum, double value)
         {
-            get { return (Brush)GetValue(HeaderBrushProperty); }
+            return (value) / (maximum - minimum) * 100;
+        }
+
+        /// <summary>
+        /// percentage value
+        /// </summary>
+        public double PercentageValue {
+            get { return (double) GetValue(PercentageValueProperty); }
+            set { SetValue(PercentageValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Percentage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PercentageValueProperty =
+            DependencyProperty.Register("PercentageValue", typeof(double), typeof(CircularProgressBar),
+                new PropertyMetadata(0d));
+
+
+        public Brush HeaderBrush {
+            get { return (Brush) GetValue(HeaderBrushProperty); }
             set { SetValue(HeaderBrushProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for HeaderBrush.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty HeaderBrushProperty =
-            DependencyProperty.Register("HeaderBrush", typeof(Brush), typeof(CircularProgressBar), new PropertyMetadata(Brushes.DimGray));
+            DependencyProperty.Register("HeaderBrush", typeof(Brush), typeof(CircularProgressBar),
+                new PropertyMetadata(Brushes.DimGray));
 
-
-
-        public double Value
-        {
-            get { return (double)GetValue(ValueProperty); }
-            set { SetValue(ValueProperty, value); }
+        public double Thickness {
+            get { return (int) GetValue(ThicknessProperty); }
+            set { SetValue(ThicknessProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ValueProperty =
-            DependencyProperty.Register("Value", typeof(double), typeof(CircularProgressBar), new PropertyMetadata(0.0)); 
-
-
+        // Using a DependencyProperty as the backing store for Thickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThicknessProperty =
+            DependencyProperty.Register("Thickness", typeof(double), typeof(CircularProgressBar),
+                new PropertyMetadata(10d));
     }
 }
