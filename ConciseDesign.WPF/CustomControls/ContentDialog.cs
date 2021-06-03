9a891fbce22764b2f379b606bfa3b4f0cb1ba243
Karl.Zhu <981975491@qq.com>
2021-06-03 12:40:15 +0800

    temp

---
+++ ConciseDesign.WPF/CustomControls/ContentDialog.cs
@@ -0,0 +1,108 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ConciseDesign.WPF.Command;
+using ConciseDesign.WPF.UserControls;
+
+namespace ConciseDesign.WPF.CustomControls
+{
+    [TemplatePart(Name = "PART_SubmitButton", Type = typeof(Button))]
+    public class ContentDialog : Control
+    {
+        public const string SubmitButtonName = "PART_SubmitButton";
+
+        static ContentDialog()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ContentDialog),
+                new FrameworkPropertyMetadata(typeof(ContentDialog)));
+        }
+
+        
+        public static readonly DependencyProperty HeaderBackgroundProperty = DependencyProperty.Register(
+            "HeaderBackground", typeof(Brush), typeof(ContentDialog), new PropertyMetadata(Brushes.DodgerBlue));
+        
+        public Brush HeaderBackground
+        {
+            get { return (Brush) GetValue(HeaderBackgroundProperty); }
+            set { SetValue(HeaderBackgroundProperty, value); }
+        }
+        
+        public object Header
+        {
+            get { return (object) GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(object), typeof(ContentDialog),
+                new PropertyMetadata(null));
+
+        public FrameworkElement Content
+        {
+            get { return (FrameworkElement) GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register("Content", typeof(FrameworkElement), typeof(ContentDialog),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// 是否显示取消按钮
+        /// </summary>
+        public bool CanCancel
+        {
+            get { return (bool) GetValue(CanCancelProperty); }
+            set { SetValue(CanCancelProperty, value); }
+        }
+
+        public static readonly DependencyProperty CanCancelProperty =
+            DependencyProperty.Register("CanCancel", typeof(bool), typeof(ContentDialog), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty DialogContentModelProperty = DependencyProperty.Register(
+            "DialogContentModel", typeof(IDialogContent), typeof(ContentDialog),
+            new PropertyMetadata(default(IDialogContent)));
+
+        public IDialogContent DialogContentModel
+        {
+            get { return (IDialogContent) GetValue(DialogContentModelProperty); }
+            set { SetValue(DialogContentModelProperty, value); }
+        }
+
+        /// <summary>
+        /// 是否显示submit按钮
+        /// </summary>
+        public bool CanSubmit
+        {
+            get { return (bool) GetValue(CanSubmitProperty); }
+            set { SetValue(CanSubmitProperty, value); }
+        }
+
+        public static readonly DependencyProperty CanSubmitProperty =
+            DependencyProperty.Register("CanSubmit", typeof(bool), typeof(ContentDialog), new PropertyMetadata(true));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var submitButton = GetTemplateChild(SubmitButtonName) as Button;
+            submitButton.Click += SubmitButtonOnClick;
+        }
+
+        private void SubmitButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            if (DialogContentModel != null)
+            {
+                if (!DialogContentModel.TrySubmit())
+                {
+                    return;
+                }
+            }
+
+            var button = sender as Button;
+            button.SubmitDialog();
+        }
+    }
+}
---
+++ ConciseDesign.WPF/CustomControls/IDialogContent.cs
@@ -0,0 +1,10 @@
+﻿namespace ConciseDesign.WPF.CustomControls
+{
+    public interface IDialogContent
+    {
+        /// <summary>
+        /// 尝试提交
+        /// </summary>
+        bool TrySubmit();
+    }
+}
